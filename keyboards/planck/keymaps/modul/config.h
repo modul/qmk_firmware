099447bfaeef59e345cbf4b0871c72b246a08a6a
Remo Giermann <mo@liberejo.de>
2019-10-16 18:57:24 +0200

    added my own keymap optimized for use with Neo2

---
+++ keyboards/planck/keymaps/modul/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+
+// MIDI options
+#define MIDI_BASIC
+#endif
+
+// Most tactile encoders have detents every 4 stages
+#define ENCODER_RESOLUTION 4
+
