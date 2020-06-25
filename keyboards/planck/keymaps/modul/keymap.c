ff1f16eb5fd8c28004941676e2148aac4b71a99b
Remo Giermann <mo@liberejo.de>
2020-06-25 11:23:13 +0200

    no need for a blank key

--- keyboards/planck/keymaps/modul/keymap.c
+++ keyboards/planck/keymaps/modul/keymap.c
@@ -44,14 +44,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift| NU-\ |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |/ Shft|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |      | GUI  |  Alt |Lower |    Space    |Raise | Alt  | CtTab| Bspc |Enter |
+ * | Ctrl | Bspc | GUI  |  Alt |Lower |    Space    |Raise | Alt  | CtTab| GUI  |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,           KC_P,    KC_LBRC,
     KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,           KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,        KC_DOT,  RSFT_T(KC_SLSH),
-    KC_LCTL,  XXXXXXX, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, RCTL_T(KC_TAB), KC_BSPC, KC_ENT
+    KC_LCTL,  KC_BSPC, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, RCTL_T(KC_TAB), KC_RGUI, KC_ENT
 ),
 
 /* Lower
